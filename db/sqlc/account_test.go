0e67cf16f261b6f835f413175b1d613daa67ce68
luckt <lucktao8@163.com>
2022-03-04 07:20:16 +0800

    add entry and transfer unit test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -47,7 +47,6 @@ func TestGetAccount(t *testing.T) {
 	require.Equal(t, account1.ID, account2.ID)
 	require.Equal(t, account1.Owner, account2.Owner)
 	require.Equal(t, account1.Balance, account2.Balance)
-	require.Equal(t, account1.Balance, account2.Balance)
 
 	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
 
