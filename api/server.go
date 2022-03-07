51ec3b3892c8f888db28f4577a61f37b63359d39
luckt <lucktao8@163.com>
2022-03-07 11:38:47 +0800

    Implement login user API that returns PASETO or JWT access token

--- api/server.go
+++ api/server.go
@@ -1,25 +1,50 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/saintlyly/gopractise/db/sqlc"
+	"github.com/saintlyly/gopractise/token"
+	"github.com/saintlyly/gopractise/util"
 )
 
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	store      db.Store
+	config     util.Config
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 
 	}
+	server.setupRouter()
+	return server, nil
+
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
@@ -27,14 +52,6 @@ func NewServer(store db.Store) *Server {
 	router.POST("/transfers", server.createTransfer)
 
 	router.POST("/users", server.createUser)
-
+	router.POST("/users/login", server.loginUser)
 	server.router = router
-
-	return server
-
-}
-
-func (server *Server) Start(address string) error {
-	return server.router.Run(address)
-
 }
